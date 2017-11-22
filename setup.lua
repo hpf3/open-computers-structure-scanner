575ff1a9c2a48dbecc5c05d3d66dd3d23aaa1b73
hpf3 <redconman007@gmail.com>
2017-11-21 18:49:27 -0800

    fixed stuff

--- setup.lua
+++ setup.lua
@@ -6,8 +6,9 @@ _blockid = ""
 _nbtroot = {}
 _hasnbt = false
 _uistate = ""
-if isfolder("/hpbuild/structures") == false then mkdir("/hpbuild/structures")
-if isfolder("/hpbuild/inventory_handlers") == false then mkdir("/hpbuild/inventory_handlers")
-if isfolder("/hpbuild/compressed") == false then mkdir("/hpbuild/compressed")
-os.execute("wget https://raw.githubusercontent.com/hpf3/open-computers-structure-scanner/master/inventory%20managers/vanilla.lua /hpbuild/inventory_handlers/vanilla.lua")
-os.execute("wget https://raw.githubusercontent.com/hpf3/open-computers-structure-scanner/master/modifiedpackage.lua /lib/package.lua")
+if isfolder("/hpbuild/structures") == false then mkdir("/hpbuild/structures") end
+if isfolder("/hpbuild/inventory_handlers") == false then mkdir("/hpbuild/inventory_handlers") end
+if isfolder("/hpbuild/compressed") == false then mkdir("/hpbuild/compressed") end
+os.execute("wget -f https://raw.githubusercontent.com/hpf3/open-computers-structure-scanner/master/inventory%20managers/vanilla.lua /hpbuild/inventory_handlers/vanilla.lua")
+os.execute("wget -f https://raw.githubusercontent.com/hpf3/open-computers-structure-scanner/master/modifiedpackage.lua /lib/package.lua")
+os.execute("wget -f https://raw.githubusercontent.com/hpf3/open-computers-structure-scanner/master/autorun.lua /autorun.lua")
